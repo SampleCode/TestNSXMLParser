563bd4655915c0ff275849233ca8bd01250fc9e4
Rock SU <wenshyansu@yahoo.com.tw>
2013-05-23 16:35:43 +0800

    Initial Commit

---
+++ TestNSXMLParser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestNSXMLParser
+//
+//  Created by Rock Su on 13/5/23.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestNSXMLParser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestNSXMLParser
+//
+//  Created by Rock Su on 13/5/23.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
